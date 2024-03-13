c5b7ea9ae399a4404af0325cc9d0c204593c5bcf
Vikash Sarraf <vsarraf15@gmail.com>
2024-03-13 23:20:53 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express();
+
+app.use(bodyParser.json({limit:'30mb', extended:true}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
+
+app.use(cors({origin:'*'}))
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,()=>{
+    console.log(`Server Started on Port ${PORT}`)
+})
