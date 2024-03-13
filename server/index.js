80222a61d0e832bdaf72830d711dbdaff093aa6f
Aryaman Gautam <120401190+aryamanGG@users.noreply.github.com>
2024-03-13 23:48:02 +0530

    mai aagya

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({limit:'30mb', extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
 
 app.use(cors({origin:'*'}))
-
+// aryaman aagya
 
 const PORT = process.env.PORT || 8080
 
