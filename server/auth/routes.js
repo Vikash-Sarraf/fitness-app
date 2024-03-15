1f5befe1d52a7d2d125ef761bf235306d6100c7e
Aryaman Gautam <120401190+aryamanGG@users.noreply.github.com>
2024-03-15 23:35:42 +0530

    login and register tested

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const router = express.Router()
 const route = () => {
-    const controller = require("./controller")
+    const controller = require("./controller")()
 
     router.post('/register', controller.register)
     router.post('login', controller.login)
+    return router
 }
 
 module.exports = route
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-
+const authRoute = require('./auth/routes')()
 dotenv.config()
 
 const app = express();
@@ -16,7 +16,7 @@ app.use(cors({origin:'*'}))
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology: true}).then(()=>{
     console.log("MongoDB connected")
 })
-// aryaman aagya
+ app.use('/auth', authRoute)
 
 const PORT = process.env.PORT || 8080
 
